6f219442a597f3f35b7c39eec94803312f8ec765
SYL <1061300519@qq.com>
2017-08-11 12:29:47 +0800

    修改xml文件路径异常

--- src/main/java/com/syl/ioc/setter_injection/TestSetter.java
+++ src/main/java/com/syl/ioc/setter_injection/TestSetter.java
@@ -2,16 +2,24 @@ package com.syl.ioc.setter_injection;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * 测试setter 方法注入
  */
 public class TestSetter {
 
     public static void main(String[] args) {
+        File directory = new File("");//设定为当前文件夹
+        String localPath = "";
 
+        localPath =  "file:"+directory.getAbsolutePath();//获取绝对路径
+        System.out.println(localPath);
         ClassPathXmlApplicationContext context = new
                 ClassPathXmlApplicationContext(
-                        "file:D:\\ideaWORK\\SpringLearn\\src\\main\\webapp\\WEB-INF\\spring-setter.xml");
+                localPath+"\\src\\spring-setter.xml");
+
         User user = (User) context.getBean("user01");
         user.eating();
         user.getFood().eaten();
